958c97fe33beade816a595e7370b421b6624d31e
mbron770 <mbronfin.tsd@gmail.com>
2023-06-02 00:30:15 -0400

    add patch request

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-// function init (){
+// function init(){
 //     const url = 'http://localhost:3000/menu'
 //     fetch(url)
 //     .then(response => response.json())
 //     .then(menu => menu.forEach((item, index) => {
-//         if (index == 0){
-//             renderItemDetails(item)
+//         if(index == 0){
+//             displayMenu(item)
 //         }
 //         renderItems(item)
-//     })).catch(e => alert(e.message))
-//     document.querySelector('#cart-form').addEventListener('submit', e => {
-//         e.preventDefault()
-//         const numberToAddText = document.querySelector('#cart-amount').value //= menu.number_in_bag
-//         const numberInCartSpan = document.querySelector('#number-in-cart')
-//         numberInCartSpan.textContent = parseInt(numberInCartSpan.textContent) + parseInt(numberToAddText)
-//         e.target.reset()
-        
+//     })).catch(error => alert(error.message))
+
+//     let cartTotal 
+
+//     document.querySelector('#cart-form').addEventListener('submit', event => {
+//         event.preventDefault()
+//         const numberToBeAdded = parseInt(document.querySelector('#cart-amount').value)
+//         cartTotal.number_in_bag += numberToBeAdded
+//         const currentNumberInCart = document.querySelector('#number-in-cart')
+//         currentNumberInCart.textContent = parseInt(currentNumberInCart.textContent) + numberToBeAdded
+//         event.target.reset()
 //     })
 
+
 //     function renderItems(item){
-//         const itemName = document.createElement('span')
-//         itemName.textContent = item.name
-//         itemName.addEventListener('click', () => renderItemDetails(item))
-//         document.querySelector('#menu-items').append(itemName)
+//         const nameSpan = document.createElement('span')
+//         nameSpan.textContent = item.name
+//         nameSpan.addEventListener('click', () => displayMenu(item))
+//         document.querySelector('#menu-items').append(nameSpan)
+   
 //     }
 
-//     function renderItemDetails(item){
-//         document.querySelector('#dish-image').src = item.image
+//     function displayMenu(item){
+//         cartTotal = item
+//         document.querySelector('#dish-image').src = item.image 
 //         document.querySelector('#dish-name').textContent = item.name
 //         document.querySelector('#dish-description').textContent = item.description
 //         document.querySelector('#dish-price').textContent = item.price
 //         document.querySelector('#number-in-cart').textContent = item.number_in_bag
 //     }
 
+    
 // }
 
 // init()
@@ -46,15 +53,36 @@ function init(){
         }
         renderItems(item)
     })).catch(error => alert(error.message))
+    
+    let number 
+
+    function displayMenu(item){
+        number = item
+        document.querySelector('#dish-image').src = item.image 
+        document.querySelector('#dish-name').textContent = item.name
+        document.querySelector('#dish-description').textContent = item.description
+        document.querySelector('#dish-price').textContent = item.price
+        document.querySelector('#number-in-cart').textContent = item.number_in_bag
+    }
 
     document.querySelector('#cart-form').addEventListener('submit', event => {
         event.preventDefault()
-        const numberToBeAdded = document.querySelector('#cart-amount').value
+        const numberToBeAdded = parseInt(document.querySelector('#cart-amount').value)
+        number.number_in_bag += numberToBeAdded
         const currentNumberInCart = document.querySelector('#number-in-cart')
-        currentNumberInCart.textContent = parseInt(currentNumberInCart.textContent) + parseInt(numberToBeAdded)
+        currentNumberInCart.textContent = parseInt(currentNumberInCart.textContent) + numberToBeAdded
+
+        fetch(`${url}/${number.id}`, {
+            method: 'PATCH', 
+            headers: {
+                'Content-Type': 'application/json', 
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(number)
+        })
+
         event.target.reset()
     })
-
 
     function renderItems(item){
         const nameSpan = document.createElement('span')
@@ -64,15 +92,8 @@ function init(){
    
     }
 
-    function displayMenu(item){
-        document.querySelector('#dish-image').src = item.image 
-        document.querySelector('#dish-name').textContent = item.name
-        document.querySelector('#dish-description').textContent = item.description
-        document.querySelector('#dish-price').textContent = item.price
-        document.querySelector('#number-in-cart').textContent = item.number_in_bag
-    }
-
     
+
 }
 
 init()
